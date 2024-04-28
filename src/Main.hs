cbcd037be7d4cac582677d812104c8d92f9307e0
Samuel Gélineau <gelisam+github@gmail.com>
2024-04-27 23:35:54 -0400

    draw horizontal lines

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Codec.Picture ( readImage )
+import Data.Foldable (toList)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Traversable (for)
@@ -10,6 +12,10 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Juicy (fromDynamicImage)
 
+
+------------
+-- Images --
+------------
 
 imagePaths :: Map Int FilePath
 imagePaths = Map.fromList
@@ -31,17 +37,110 @@ imagePaths = Map.fromList
   , (225, "images/225.png")
   ]
 
-
-
-main :: IO ()
-main = do
-  putStrLn "Loading images..."
-  pics <- for imagePaths $ \path -> do
+loadImages :: IO (Map Int Picture)
+loadImages = do
+  for imagePaths $ \path -> do
     readImage path >>= \case
       Left err -> fail err
       Right img -> case fromDynamicImage img of
         Nothing -> fail "Unsupported image format"
         Just pic -> pure pic
 
-  let window = InWindow "acme-tiny-rules" (800, 600) (10, 10)
-  display window white (pics Map.! 15)
+
+-----------
+-- State --
+-----------
+
+data Rule = Rule
+  { rule_x0
+      :: Float
+  , rule_x1
+      :: Float
+  , rule_y
+      :: Float
+  }
+
+setX1 :: Float -> Rule -> Rule
+setX1 x1 rule
+  = rule { rule_x1 = x1 }
+
+data World = World
+  { world_rules
+      :: [Rule]
+  , world_currentRule
+      :: Maybe Rule
+  }
+
+initialWorld :: World
+initialWorld = World
+  { world_rules = []
+  , world_currentRule = Nothing
+  }
+
+
+--------------
+-- Handlers --
+--------------
+
+handleEvent :: Event -> World -> World
+handleEvent (EventKey (MouseButton LeftButton) Down _ (x, y)) world
+  = world
+  { world_currentRule
+      = Just
+      $ Rule x x y
+  }
+handleEvent (EventKey (MouseButton LeftButton) Up _ (_, _)) world@(World {..})
+  = world
+  { world_rules
+      = toList world_currentRule
+     ++ world_rules
+  , world_currentRule
+      = Nothing
+  }
+handleEvent (EventMotion (x, _)) world@(World {..})
+  = world
+  { world_currentRule
+      = fmap (setX1 x) world_currentRule
+  }
+handleEvent _ world
+  = world
+
+handleTick :: Float -> World -> World
+handleTick _ world = world
+
+
+-------------
+-- Display --
+-------------
+
+renderRule :: Rule -> Picture
+renderRule (Rule {..}) = Line
+  [ (rule_x0, rule_y)
+  , (rule_x1, rule_y)
+  ]
+
+renderWorld :: World -> Picture
+renderWorld (World {..}) = Pictures
+  $ fmap renderRule (toList world_currentRule)
+ ++ map renderRule world_rules
+
+
+----------
+-- Main --
+----------
+
+main :: IO ()
+main = do
+  putStrLn "Loading images..."
+  pics <- loadImages
+  putStrLn "Images loaded."
+
+  play
+    (InWindow "Tiny Rules" (500, 500) (10, 10))
+    white
+    30
+    initialWorld
+    renderWorld
+    handleEvent
+    handleTick
+
